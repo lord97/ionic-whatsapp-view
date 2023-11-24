eab4d892ce10b3aeefabfdccd17e6fd9c7dff8be
lord97 <sanoumohamed97@gmail.com>
2023-11-24 13:46:00 +0000

    first commit

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -9,4 +9,55 @@ export class Tab1Page {
 
   constructor() {}
 
+  EventList = [
+    {
+      'id' : 1,
+      'libelle' : 'Mobile app training with Ionic',
+      'date' : "du 20 - 24 nov 2023",
+      'place' : 'ODC Bobo Club'
+    },
+    {
+      'id' : 2,
+      'libelle' : 'Destok app training with Ionic',
+      'date' : "du 20 - 24 nov 2023",
+      'place' : 'ODC Bobo Club',
+    },
+    {
+      'id' : 3,
+      'libelle' : 'Mobile app training with Ionic',
+      'date' : "du 20 - 24 nov 2023",
+      'place' : 'ODC Bobo Club',
+    },
+    {
+      'id' : 4,
+      'libelle' : 'Mobile app training with Ionic',
+      'date' : "du 20 - 24 nov 2023",
+      'place' : 'ODC Bobo Club',
+    },
+    {
+      'id' : 1,
+      'libelle' : 'Mobile app training with Ionic',
+      'date' : "du 20 - 24 nov 2023",
+      'place' : 'ODC Bobo Club'
+    },
+    {
+      'id' : 2,
+      'libelle' : 'Destok app training with Ionic',
+      'date' : "du 20 - 24 nov 2023",
+      'place' : 'ODC Bobo Club',
+    },
+    {
+      'id' : 3,
+      'libelle' : 'Mobile app training with Ionic',
+      'date' : "du 20 - 24 nov 2023",
+      'place' : 'ODC Bobo Club',
+    },
+    {
+      'id' : 4,
+      'libelle' : 'Mobile app training with Ionic',
+      'date' : "du 20 - 24 nov 2023",
+      'place' : 'ODC Bobo Club',
+    }
+  ];
+
 }
